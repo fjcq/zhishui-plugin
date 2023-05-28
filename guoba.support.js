2c2bd80b9813592e97a2df3183de9a2ad8429bf1
fjcq <fjcq@qq.com>
2023-05-28 17:34:52 +0800

    更新锅巴

--- guoba.support.js
+++ guoba.support.js
@@ -34,7 +34,8 @@ export function supportGuoba() {
                     label: '播放器',
                     bottomHelpMessage: '设置[#看剧]使用的播放器',
                     component: 'Input'
-                }, {
+                },
+                {
                     component: 'Divider',
                     label: '对话设置'
                 },
@@ -42,6 +43,18 @@ export function supportGuoba() {
                     field: 'duihua.NickName',
                     label: '对话昵称',
                     bottomHelpMessage: '对话触发昵称',
+                    component: 'Input'
+                },
+                {
+                    field: 'duihua.Master',
+                    label: '主人名字',
+                    bottomHelpMessage: '场景对话中机器人的主人名字',
+                    component: 'Input'
+                },
+                {
+                    field: 'duihua.MasterQQ',
+                    label: '主人QQ',
+                    bottomHelpMessage: '场景对话中机器人的主人QQ',
                     component: 'Input'
                 },
                 {
@@ -69,12 +82,6 @@ export function supportGuoba() {
                     component: 'Input'
                 },
                 {
-                    field: 'duihua.EnableBing',
-                    label: '必应开关',
-                    bottomHelpMessage: '是否开启必应对话',
-                    component: 'Switch'
-                },
-                {
                     field: 'duihua.EnableVoice',
                     label: '对话语音',
                     bottomHelpMessage: '是否开启对话语音',
@@ -93,23 +100,43 @@ export function supportGuoba() {
                     },
                 },
                 {
-                    field: 'duihua.Master',
-                    label: '主人名字',
-                    bottomHelpMessage: '场景对话中机器人的主人名字',
+                    field: 'duihua.toneStyle',
+                    label: '必应模型',
+                    bottomHelpMessage: '必应对话的语言模型',
+                    component: 'Select',
+                    componentProps: {
+                      options: [
+                        { label: '默认', value: 'balanced' },
+                        { label: '创意', value: 'creative' },
+                        { label: '精确', value: 'precise' },
+                        { label: '快速', value: 'fast' }
+                      ],
+                      placeholder: '请选择语言模型'
+                    }
+                  },
+                {
+                    component: 'Divider',
+                    label: '代理设置'
+                },
+                {
+                    field: 'proxy.proxyAddress',
+                    label: '代理地址',
+                    bottomHelpMessage: '设置搜剧和对话中使用的代理地址',
                     component: 'Input'
                 },
                 {
-                    field: 'duihua.MasterQQ',
-                    label: '主人QQ',
-                    bottomHelpMessage: '场景对话中机器人的主人QQ',
-                    component: 'Input'
+                    field: 'proxy.switchProxy',
+                    label: '开启代理',
+                    bottomHelpMessage: '是否允许在搜剧和对话中使用代理',
+                    component: 'Switch'
                 },
             ],
 
             getConfigData() {
                 return {
                     souju: Config.SearchVideos,
-                    duihua: Config.Chat
+                    duihua: Config.Chat,
+                    proxy: Config.proxy
                 }
             },
 
