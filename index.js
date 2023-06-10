3e83a7748400eaba17a0e5921241cc0bebc39d12
fjcq <fjcq@qq.com>
2023-06-10 20:59:12 +0800

    优化插件载入逻辑

--- index.js
+++ index.js
@@ -1,51 +1,45 @@
 import Ver from './components/Version.js'
-import fs from "node:fs"
+import fs from 'fs'
 import chalk from 'chalk'
-
-//检查依赖
-import {checkPackage} from './components/check.js'
-let passed = await checkPackage()
-if (!passed) {
-  throw 'Missing necessary dependencies'
-}
-
-const files = fs
-    .readdirSync("./plugins/zhishui-plugin/apps")
-    .filter((file) => file.endsWith(".js"));
-
-let ret = []
+import Data from './components/Data.js'
 
 logger.info(chalk.yellow(`正在加载 止水插件${Ver.ver}`))
 
+//检查依赖
+import { checkPackage } from './components/check.js'
+let passed = await checkPackage()
+if (!passed) {
+    throw 'Missing necessary dependencies'
+}
 
+if (!global.segment) {
+    try {
+        global.segment = (await import('oicq')).segment
+    } catch (err) {
+        global.segment = (await import('icqq')).segment
+    }
+}
+
+let ret = []
+const files = fs
+    .readdirSync('./plugins/zhishui-plugin/apps')
+    .filter((file) => file.endsWith('.js'))
 
 files.forEach((file) => {
     ret.push(import(`./apps/${file}`))
 })
 
-if (!global.segment) {
-    global.segment = (await import("oicq")).segment
-  }
-  
-  if (!global.core) {
-    try {
-      global.core = (await import("oicq")).core
-    } catch (err) {}
-  }
-
 ret = await Promise.allSettled(ret)
 
 let apps = {}
 for (let i in files) {
-    let name = files[i].replace(".js", "")
+    let name = files[i].replace('.js', '')
 
-    if (ret[i].status != "fulfilled") {
-        logger.error("载入插件错误：" + logger.red(name))
+    if (ret[i].status != 'fulfilled') {
+        logger.error(`载入插件错误：${logger.red(name)}`)
         logger.error(ret[i].reason)
         continue
     }
-
     apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
 }
-
 export { apps }
